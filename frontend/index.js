de11e4e71e9a16d02b923a5e574c8a3d43ae4e2f
markdrehmann <markdrehmann@gmail.com>
2021-04-22 11:09:36 -0500

    Added function 'getLists' in index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,9 @@
 function test() {
   console.log("test text")
 }
+
+function getLists() {
+  fetch("http://localhost:3000/lists")
+    .then(res => res.json())
+    .then(data => data.forEach(list => console.log(list)))
+}
